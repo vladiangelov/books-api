710cc44aafe1a1881d27417f4ac55aa6d7bc76a1
Vladi Angelov <vladislav_angelov@yahoo.com>
2020-05-02 17:38:34 +0100

    Added POST endpoint for creating an author (without authorisation yet)

--- app/controllers/api/v1/authors_controller.rb
+++ app/controllers/api/v1/authors_controller.rb
@@ -13,10 +13,29 @@ class Api::V1::AuthorsController < Api::V1::BaseController
     @books
   end
 
+  def create
+    @author = Author.new(author_params)
+    authorize @author
+    if @author.save
+      render :show, status: :created
+    else
+      render_error
+    end
+  end
+
   private
 
   def set_author
     @author = Author.find(params[:id])
     authorize @author # For Pundit
+  end
+
+  def author_params
+    params.require(:author).permit(:name)
+  end
+
+  def render_error
+    render json: { errors: @author.errors.full_messages },
+           status: :unprocessable_entity
   end
 end
--- app/policies/author_policy.rb
+++ app/policies/author_policy.rb
@@ -8,4 +8,8 @@ class AuthorPolicy < ApplicationPolicy
   def show?
     true
   end
+
+  def create?
+    true
+  end
 end
